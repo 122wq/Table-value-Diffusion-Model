cc4809714f59f1b9602ceecd8c40941695bf6d45
122wq <jackwanghaoran@outlook.com>
2026-07-01 22:36:16 +0800

    Updated risk prediction

--- diffusion_gradio.py
+++ diffusion_gradio.py
@@ -16,14 +16,21 @@ def greet(p1, p2, p3 , p4, p5, p6, p7):
     output_fake= outputs[0]
     output_fake = softmax(output_fake, axis = 1)
     outputs = output_fake[:,1]
-    return outputs[0]
+    outputs = outputs[0]
+    if (outputs > 0.692):
+        risk = "High Risk"
+    elif (outputs > 0.515):
+        risk = "Medium Risk"
+    else:
+        risk = "Low Risk"
+    return outputs * 100, risk
 
 project = gr.Interface(
     fn=greet,
     inputs=[gr.Number(label="Clinical Systolic Blood Pressure"), gr.Number(label="Clinical DBP"),
              gr.Number(label="eGFR"), gr.Number(label="Body Mass Index"), 
              gr.Number(label="nRAAs Drug Use"), gr.Number(label="History of Hypertension"), gr.Number(label="Age")],
-    outputs=[gr.Number(label="NH Prediction")],
+    outputs=[gr.Number(label="NH Prediction (%)"), gr.Textbox(label="Patient Risk")],
     api_name="predict"
 )
 project.launch(share=False)
