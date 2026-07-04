a363a24f4a47736cb2f8a74c4caf440228e5d6c2
122wq <jackwanghaoran@outlook.com>
2026-07-04 22:21:37 +0800

    modified:   diffusion_gradio.py

--- diffusion_gradio.py
+++ diffusion_gradio.py
@@ -6,23 +6,30 @@ from scipy.special import softmax
 sess = ort.InferenceSession("onnx_diffusion.onnx")
 
 def greet(p1, p2, p3 , p4, p5, p6, p7):
+    #define outputs from the model
     outputs = sess.run(
     None,
     {
+        #7 selected varibles as np array
         "cond": np.array([[p1, p2, p3, p4, p5, p6, p7]]).astype(np.float32),
         "t": np.array([500], dtype=np.float32),
     }
 )
+    #raw logit
     output_fake= outputs[0]
+    #calculated output
     output_fake = softmax(output_fake, axis = 1)
+    #convert to a single layer array
     outputs = output_fake[:,1]
     outputs = outputs[0]
+    #classify the patient risk based on calculated output percentage
     if (outputs > 0.692):
         risk = "High Risk"
     elif (outputs > 0.515):
         risk = "Medium Risk"
     else:
         risk = "Low Risk"
+    #return the NH percentage and risk classification
     return outputs * 100, risk
 
 project = gr.Interface(
